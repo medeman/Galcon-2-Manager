17e8f22cc82fd804be2455307a34b874307cd0b2
Medeman <medemanx@googlemail.com>
2015-03-21 17:32:29 +0100

    Added two options to settings form, settings can be saved now

--- Galcon 2 Manager/Settings.Designer.cs
+++ Galcon 2 Manager/Settings.Designer.cs
@@ -28,22 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.labelSettingHashLatestUrl = new System.Windows.Forms.Label();
+            this.labelSettingDownloadUrl = new System.Windows.Forms.Label();
+            this.textBoxSettingHashLatestUrl = new System.Windows.Forms.TextBox();
+            this.textBoxSettingDownloadUrl = new System.Windows.Forms.TextBox();
+            this.buttonSave = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // labelSettingHashLatestUrl
+            // 
+            this.labelSettingHashLatestUrl.AutoSize = true;
+            this.labelSettingHashLatestUrl.Location = new System.Drawing.Point(12, 41);
+            this.labelSettingHashLatestUrl.Name = "labelSettingHashLatestUrl";
+            this.labelSettingHashLatestUrl.Size = new System.Drawing.Size(162, 13);
+            this.labelSettingHashLatestUrl.TabIndex = 0;
+            this.labelSettingHashLatestUrl.Text = "URL of hash of the latest version";
+            // 
+            // labelSettingDownloadUrl
+            // 
+            this.labelSettingDownloadUrl.AutoSize = true;
+            this.labelSettingDownloadUrl.Location = new System.Drawing.Point(12, 15);
+            this.labelSettingDownloadUrl.Name = "labelSettingDownloadUrl";
+            this.labelSettingDownloadUrl.Size = new System.Drawing.Size(124, 13);
+            this.labelSettingDownloadUrl.TabIndex = 1;
+            this.labelSettingDownloadUrl.Text = "URL to the latest version";
+            // 
+            // textBoxSettingHashLatestUrl
+            // 
+            this.textBoxSettingHashLatestUrl.Location = new System.Drawing.Point(180, 38);
+            this.textBoxSettingHashLatestUrl.Name = "textBoxSettingHashLatestUrl";
+            this.textBoxSettingHashLatestUrl.Size = new System.Drawing.Size(330, 20);
+            this.textBoxSettingHashLatestUrl.TabIndex = 2;
+            // 
+            // textBoxSettingDownloadUrl
+            // 
+            this.textBoxSettingDownloadUrl.Location = new System.Drawing.Point(180, 12);
+            this.textBoxSettingDownloadUrl.Name = "textBoxSettingDownloadUrl";
+            this.textBoxSettingDownloadUrl.Size = new System.Drawing.Size(330, 20);
+            this.textBoxSettingDownloadUrl.TabIndex = 3;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(435, 166);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 4;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
             // 
             // Settings
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(522, 201);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.textBoxSettingDownloadUrl);
+            this.Controls.Add(this.textBoxSettingHashLatestUrl);
+            this.Controls.Add(this.labelSettingDownloadUrl);
+            this.Controls.Add(this.labelSettingHashLatestUrl);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Settings";
             this.Text = "Settings";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelSettingHashLatestUrl;
+        private System.Windows.Forms.Label labelSettingDownloadUrl;
+        private System.Windows.Forms.TextBox textBoxSettingHashLatestUrl;
+        private System.Windows.Forms.TextBox textBoxSettingDownloadUrl;
+        private System.Windows.Forms.Button buttonSave;
     }
 }
--- Galcon 2 Manager/Settings.cs
+++ Galcon 2 Manager/Settings.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
 
 namespace Galcon_2_Manager
 {
@@ -15,6 +16,19 @@ namespace Galcon_2_Manager
         public Settings()
         {
             InitializeComponent();
+
+            textBoxSettingDownloadUrl.Text = ConfigurationManager.AppSettings["downloadUrl"];
+            textBoxSettingHashLatestUrl.Text = ConfigurationManager.AppSettings["hashLatestUrl"];
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+            config.AppSettings.Settings["downloadUrl"].Value = textBoxSettingDownloadUrl.Text;
+            config.AppSettings.Settings["hashLatestUrl"].Value = textBoxSettingHashLatestUrl.Text;
+
+            config.Save(ConfigurationSaveMode.Modified);
         }
     }
 }
